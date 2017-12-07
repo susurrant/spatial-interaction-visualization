54092397d4d1c5d178f67b78a70676f490fc9700
Xin Yao <susurrant@outlook.com>
2017-12-07 22:10:38 +0800

    add diagramMap

--- odmap.py
+++ odmap.py
@@ -8,15 +8,15 @@ from sklearn.cluster import KMeans
 # 读取五环内的交互
 def readData(filename, rows, columns, minSpeed=2, maxSpeed=150):
     data = np.zeros((rows**2, columns**2), dtype=np.uint32)
-
     with open(filename, 'r') as f:
         f.readline()
         while True:
             line1 = f.readline().strip()
-            if line1:
+            line2 = f.readline().strip()
+            if line1 and line2:
                 sl1 = line1.split(',')
-                sl2 = f.readline().strip().split(',')
-                if int(sl1[1]) == 0 or int(sl1[1]) == 0:
+                sl2 = line2.split(',')
+                if int(sl1[1]) == 0 or int(sl2[1]) == 0:
                     continue
                 if float(sl1[-2]) < minSpeed or float(sl1[-2]) > maxSpeed:
                     continue
@@ -33,7 +33,6 @@ def readData(filename, rows, columns, minSpeed=2, maxSpeed=150):
                 data[ridx, cidx] += 1
             else:
                 break
-
     return data
 
 # kmeans classifier
@@ -54,7 +53,6 @@ def drawODMap(file_name, save_file_name, ia):
 
     data = readData(file_name, ia['rows'], ia['columns'])
     nk, nl = kmeans(data.flatten(), ia['class_number'])
-    print(data[14,30])
     gridWidth = ia['gridWidth']
 
     for r in range(ia['rows']**2):
@@ -70,9 +68,17 @@ def drawODMap(file_name, save_file_name, ia):
             left = ia['ox'] + (c - ia['xoffset'])*gridWidth*ia['columns']
             draw.rectangle([(left, top), (left+gridWidth*ia['columns'], top+gridWidth*ia['rows'])], outline=ia['border_color'])
 
-    iquality = ia['quality']
-    idpi = ia['dpi']
-    image.save(save_file_name, quality=iquality, dpi=idpi)
+    left = (ia['width'] - ia['legend_width']*ia['class_number']) // 2
+    bottom = ia['height'] - 10
+    for i, c in enumerate(ia['color_scheme']):
+        draw.rectangle([(left+i*ia['legend_width'], bottom-ia['legend_height']), (left+(i+1)*ia['legend_width'], bottom)],
+                       fill=c)
+
+    imageTitlefont = ImageFont.truetype('./font/times.ttf', 58)
+    draw.text((left - 50, bottom - ia['legend_height']), '0', font=imageTitlefont, fill=(0, 0, 0))
+    draw.text((left + ia['legend_width']*ia['class_number'] + 20, bottom - ia['legend_height']), 'max magnitude', font=imageTitlefont, fill=(0, 0, 0))
+
+    image.save(save_file_name, quality=ia['quality'], dpi=ia['dpi'])
 
 # 读取渲染设置
 def readDrawingSetting():
@@ -85,18 +91,19 @@ def readDrawingSetting():
     #   聚类数：class_number；颜色梯度：color_scheme
     ia = dict()
     ia.update({'rows': 15, 'columns':15,
-               'gridWidth': 13, 'ox': 30, 'oy': 40,
-               'width': 3000, 'height': 3000, 'xoffset': 0, 'yoffset': 0,
-               'legend_size': 63, 'legend_yoffset': 130, 'legend_xoffset': 150,
-               'class_number': 15, 'quality': 1000, 'dpi': (1200, 1200)})
+               'gridWidth': 15, 'ox': 20, 'oy': 20,
+               'width': 3415, 'height': 3500, 'xoffset': 0, 'yoffset': 0,
+               'legend_height': 80, 'legend_width': 60,
+               'quality': 1000, 'dpi': (1200, 1200)})
 
     # color setting
     ia['border_color'] = '#000000'
-    cstr = 'fefffe#fff2f1#ffe5e3#ffd9d5#ffccc6#ffc0b8#ffb3aa#ffa69b#ff9a8c#ff8d7d#ff806d#ff735c#ff6548#ff5730#fe4800'
+    cstr = 'ffffff#ffff80#fff771#ffee61#ffe452#ffd743#ffc933#ffb924#ffa815#ff9505#f58000#e66c00#d65900#c74900#b83900#a82c00#991f00#8a1500#7a0c00#6b0500'
+    cstr = 'ffffff#f5f500#f5e700#f5da00#f5cc00#f5bf00#f5b100#f5a300#f59600#f58800#f57a00#f56d00#f55f00#f55200#f54400#f53600#f52900#f51b00#f50e00#f50000'
     ia['color_scheme'] = []
     for color in cstr.split('#'):
         ia['color_scheme'].append('#'+color)
-
+    ia['class_number'] = len(ia['color_scheme'])
     return ia
 
 
